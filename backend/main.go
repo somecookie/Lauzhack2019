430665546afdf715cad439b91b2855a3bc3ff040
Ricardo <ricardo.ferreiraribeiro@protonmail.com>
2019-11-16 16:53:07 +0100

    Add search endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
-	"github.com/somecookie/Peerster/helper"
+	"fmt"
+	"log"
 	"net/http"
+	"Lauzhack2019/backend/search"
 )
 
 func main() {
 	serverAddr := "localhost:8080"
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
-	//http.HandleFunc("/id", IDHandler)
+	http.HandleFunc("/search", searchHandler)
 
 	for {
-		err := http.ListenAndServe(serverAddr, nil)
-		helper.LogError(err)
+		if err := http.ListenAndServe(serverAddr, nil); err != nil{
+			log.Println(err)
+		}
+
+	}
+}
+
+func searchHandler(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method{
+	case "GET":
+		search.Query{}
+		if firstName, ok := request.URL.Query()["firstName"]; ok{
+			fmt.Println(firstName)
+		}
+
+
+	default:
+		writer.WriteHeader(http.StatusNotFound)
 	}
 }
